16de340873fdb1e106d17635b9a959f3cbbf13d5
ahmadalkp <ahmadalkp@gmail.com>
2023-02-15 11:35:15 +0700

    kayaknya bisa

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,8 @@ import { Angulartics2GoogleAnalytics } from 'angulartics2';
 import { Angulartics2 } from 'angulartics2';
 import { Angulartics2GoogleAnalyticsEnhancedEcommerce } from 'angulartics2';
 
+declare var ga: Function; // declare the ga function
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -14,26 +16,40 @@ export class AppComponent implements OnInit {
   constructor(
     private angulartics2GoogleAnalytics: Angulartics2GoogleAnalytics,
     private angulartics2: Angulartics2,
-    private angulartics2GoogleAnalyticsEnhancedEcommerce: Angulartics2GoogleAnalyticsEnhancedEcommerce) {
+  ) {
     angulartics2GoogleAnalytics.startTracking();
   }
 
   ngOnInit(): void {
+    ga('require', 'ec');
   }
 
   test() {
     this.angulartics2.eventTrack.next({
-      action: 'myAction',
+      action: "action",
       properties: {
-        category: 'myCategory',
-        label: 'myLabel',
-      },
-    });
+        category: "category",
+        label: "label"
+      }
+    }
+    )
+  }
+
+  ecommerce() {
+    this.angulartics2.eventTrack.next({
+      action: "send",
+      properties: {
+        event: "event",
+        category: "checkout",
+        label: "label"
+      }
+    }
+    )
   }
 
   analytics(param: any) {
     if (param == "ecAddImpression") {
-      this.angulartics2GoogleAnalyticsEnhancedEcommerce.ecAddImpression({
+      Angulartics2GoogleAnalyticsEnhancedEcommerce.prototype.ecAddImpression({
         id: '123123',
         name: 'test',
         list: 'list',
@@ -44,7 +60,7 @@ export class AppComponent implements OnInit {
         price: 10000,
       });
     } else if (param == "ecAddProduct") {
-      this.angulartics2GoogleAnalyticsEnhancedEcommerce.ecAddProduct({
+      Angulartics2GoogleAnalyticsEnhancedEcommerce.prototype.ecAddProduct({
         id: '123123',
         name: 'test',
         brand: 'brand',
@@ -55,6 +71,16 @@ export class AppComponent implements OnInit {
         coupon: 'coupon',
         position: 12,
       });
+    } else {
+      Angulartics2GoogleAnalyticsEnhancedEcommerce.prototype.ecSetAction('purchase', {          // Transaction details are provided in an actionFieldObject.
+        'id': 'T12345',                         // (Required) Transaction id (string).
+        'affiliation': 'Google Store - Online', // Affiliation (string).
+        'revenue': 10000000,                     // Revenue (number).
+        'tax': 1000,                          // Tax (number).
+        'shipping': 50000,                     // Shipping (number).
+        'coupon': 'SUMMER2013'                  // Transaction coupon (string).
+      });
+
     }
   }
 }
